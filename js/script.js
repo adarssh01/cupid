4ee2548d3d17c555b879f3b2ae4e91c1887eb4fb
Adarsh prasad <98263616+adarssh01@users.noreply.github.com>
2023-04-05 12:44:55 +0530

    added circle animation

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ const observer = new IntersectionObserver((entries) => {
   elements.forEach((element) => {
     observer.observe(element);
   });
+
+  // Get all the circles
+var circles = document.querySelectorAll('.timeline__circle');
+
+// Add data-aos attribute to each circle
+circles.forEach(function(circle, index) {
+  circle.setAttribute('data-aos', 'fade-in');
+  circle.setAttribute('data-aos-delay', index * 200);
+});
+
+// Initialize AOS with desired options
+AOS.init({
+  offset: 100,
+  duration: 800,
+});
